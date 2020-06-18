90ec87217ebae204c46c76f511d01dfaf99edd37
hrmq <h.rahimi.moqadam@gmail.com>
2020-06-18 16:21:50 -0400

    Updates

---
+++ precache-manifest.091b26428dcbd446014c578b36157212.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da15ffa82ba159130fa6f26f9508134c",
+    "url": "/react-rolodex/index.html"
+  },
+  {
+    "revision": "de9755e8a0e43398cc52",
+    "url": "/react-rolodex/static/css/main.455ebe29.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/react-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de9755e8a0e43398cc52",
+    "url": "/react-rolodex/static/js/main.c0af2d57.chunk.js"
+  },
+  {
+    "revision": "bff0e6aef3ce725fc2ff",
+    "url": "/react-rolodex/static/js/runtime-main.99be1617.js"
+  }
+]);
